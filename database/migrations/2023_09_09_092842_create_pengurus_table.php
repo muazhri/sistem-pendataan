19c41c5403235538c62faeb9f5c010adf48cc1e3
muazhri <muhaiminaziz216@gmail.com>
2023-09-09 16:36:30 +0700

    first commit

---
+++ database/migrations/2023_09_09_092842_create_pengurus_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengurus', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('photo');
+            $table->string('gender');
+            $table->string('address');
+            $table->string('place_birth');
+            $table->date('date_birth');
+            $table->date('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengurus');
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title', 'Homepage')
+@section('content')
+    
+@endsection
